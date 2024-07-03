6c38349662a4d3ef1cfc66fe3a3d34f8a9b8c530
Yusuf <usuf7hahin@gmail.com>
2024-07-03 18:57:32 +0600

    third commit, I just ready my 02-menu file

--- 02-menu/app.js
+++ 02-menu/app.js
@@ -1,5 +1,6 @@
 const sectionCenter = document.querySelector('.section-center');
 
+// load items
 window.addEventListener('DOMContentLoaded', () => {
   // 'menu' basically come from 'manu.js'
   addMenu(menu);
@@ -21,10 +22,33 @@ const addMenu = (menuItems) => {
         </article>`;
   });
   // console.log(displayManu);
+
   // =============
   // join("") is a array method which convert a array into string
+  // '' === in string there is no gap between array index , for example : ['apple','banana','mango'].join('') === 'applebananamango';
   // =============
   displayManu = displayManu.join('');
   // console.log(displayManu);
   sectionCenter.innerHTML = displayManu;
 };
+
+// filter items
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+filterBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    // console.log(e.currentTarget.dataset.id);
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter((menuItems) => {
+      if (menuItems.category === category) {
+        return menuItems;
+      }
+    });
+    // console.log(menuCategory);
+    if (category === 'all') {
+      addMenu(menu);
+    } else {
+      addMenu(menuCategory);
+    }
+  });
+});

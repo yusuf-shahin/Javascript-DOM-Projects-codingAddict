211fd19d2ff971da5ceedf92dfe1a46e16ebf2e8
Yusuf <usuf7hahin@gmail.com>
2024-07-11 20:14:08 +0600

    11-modal project is completed , 10-grocery-bud project is running

--- 10-grocery-bud/app.js
+++ 10-grocery-bud/app.js
@@ -15,7 +15,11 @@ let editId = '';
 
 // ****** EVENT LISTENERS **********
 
+// submit form
 form.addEventListener('click', addItem);
+
+// clear btn
+clearBtn.addEventListener('click', clearItem);
 
 function addItem(e) {
   e.preventDefault();
@@ -26,17 +30,48 @@ function addItem(e) {
 
   if (inputValue !== '' && editFlag === false) {
     // condition shortcut === (inputValue && !editFlag)
-    console.log('add item to the list');
+    const element = document.createElement('article');
+    element.classList.add('grocery-item');
+    // add id
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+
+    // inner html set
+    element.innerHTML = `<p class="title">${inputValue}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+
+    // append child
+    list.appendChild(element);
+
+    // display alert function
+    displayAlert('item is added', 'success');
+
+    // show container
+    container.classList.add('show-container');
+
+    // add to local storge
+    addToLocalStorage(id, inputValue);
+    // set back to default
+    setBackToDefaule();
   } else if (inputValue !== '' && editFlag === true) {
     // condition shortcut === (inputValue && editFlag)
     console.log('item is editing');
   } else {
-    displayAlert('please enter the value', 'danger');
+    displayAlert('please enter the item', 'danger');
   }
 }
 
-// alert function
+// ****** FUNCTIONS **********
 
+// alert function
 function displayAlert(text, action) {
   alert.textContent = text;
   alert.classList.add(`alert-${action}`);
@@ -47,8 +82,32 @@ function displayAlert(text, action) {
     alert.classList.remove(`alert-${action}`);
   }, 1000);
 }
+// set back to default
+function setBackToDefaule() {
+  groceryInput.value = '';
+  editFlag = false;
+  editId = '';
+  submitBtn.textContent = 'submit';
+}
 
-// ****** FUNCTIONS **********
+// clear item function
+function clearItem() {
+  const inputItems = document.querySelectorAll('.grocery-item');
+  if (inputItems.length > 0) {
+    inputItems.forEach((item) => {
+      list.removeChild(item);
+    });
+  }
+  container.classList.remove('show-container');
+  displayAlert('empty item', 'danger');
+
+  // local storage remove list
+  setBackToDefaule();
+}
+
+function addToLocalStorage(id, value) {
+  console.log('add to local storage');
+}
 
 // ****** LOCAL STORAGE **********
 
---
+++ 11-modal/app.js
@@ -0,0 +1,11 @@
+const modalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', () => {
+  modalOverlay.classList.add('open-modal');
+});
+
+closeBtn.addEventListener('click', () => {
+  modalOverlay.classList.remove('open-modal');
+});
